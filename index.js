97596944e55f8cc3364d0d393dd1607bed559539
haoge deng <haoge.deng@gmail.com>
2019-08-12 19:58:35 +1000

    change back to backend

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// const http = require('http');
-// const httpServer = http.createServer();
-// var echo = require('socket.io')(httpServer);
-
-// echo.on('connection', connection => {
-//     connection.on('message', message => {
-//         console.log('a user connected!');
-//         console.log(`Data from client: ${message}`);
-//         echo.emit('message', message, {for: 'everyone'});
-//     });
-
-//     connection.on('disconnect', () => {
-//         console.log('connection closed!');
-//     });
-
-// });
-
-// httpServer.listen(process.env.PORT || 9999);
-
 const http = require('http');
+const httpServer = http.createServer();
+var echo = require('socket.io')(httpServer);
 
-const server = http.createServer((req, res) => {
-    res.write('Hi, welcome to my first azure app!');
-    res.end();
+echo.on('connection', connection => {
+    connection.on('message', message => {
+        console.log('a user connected!');
+        console.log(`Data from client: ${message}`);
+        echo.emit('message', message, {for: 'everyone'});
+    });
+
+    connection.on('disconnect', () => {
+        console.log('connection closed!');
+    });
+
 });
 
-server.listen(process.env.PORT || 3000);
+httpServer.listen(process.env.PORT || 3000);
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.write('Hi, welcome to my first azure app!');
+//     res.end();
+// });
+
+// server.listen(process.env.PORT || 3000);
