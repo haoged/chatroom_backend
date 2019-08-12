927366b29ba715bc5f33b778f5696988c7aa183b
haoge deng <haoge.deng@gmail.com>
2019-08-12 20:54:16 +1000

    enable cross origin

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const http = require('http');
 const httpServer = http.createServer();
-var echo = require('socket.io')(httpServer);
+var echo = require('socket.io')(httpServer, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 
 echo.on('connection', connection => {
     connection.on('message', message => {
         console.log('a user connected!');
         console.log(`Data from client: ${message}`);
-        echo.emit('message', message, {for: 'everyone'});
+        echo.emit('message', message, { for: 'everyone' });
     });
 
     connection.on('disconnect', () => {
@@ -15,7 +25,7 @@ echo.on('connection', connection => {
 
 });
 
-httpServer.listen(process.env.PORT || 3000);
+httpServer.listen(process.env.PORT || 9999);
 
 // const http = require('http');
 
