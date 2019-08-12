aa0239c900fc680fcf8fbd8a2416c0dc06fdc1d2
haoge deng <haoge.deng@gmail.com>
2019-08-12 19:55:15 +1000

    change to server side rendering to test

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+// const http = require('http');
+// const httpServer = http.createServer();
+// var echo = require('socket.io')(httpServer);
+
+// echo.on('connection', connection => {
+//     connection.on('message', message => {
+//         console.log('a user connected!');
+//         console.log(`Data from client: ${message}`);
+//         echo.emit('message', message, {for: 'everyone'});
+//     });
+
+//     connection.on('disconnect', () => {
+//         console.log('connection closed!');
+//     });
+
+// });
+
+// httpServer.listen(process.env.PORT || 9999);
+
 const http = require('http');
-const httpServer = http.createServer();
-var echo = require('socket.io')(httpServer);
 
-echo.on('connection', connection => {
-    connection.on('message', message => {
-        console.log('a user connected!');
-        console.log(`Data from client: ${message}`);
-        echo.emit('message', message, {for: 'everyone'});
-    });
-
-    connection.on('disconnect', () => {
-        console.log('connection closed!');
-    });
-
+const server = http.createServer((req, res) => {
+    res.write('Hi, welcome to my first azure app!');
+    res.end();
 });
 
-httpServer.listen(process.env.PORT || 9999);
+server.listen(process.env.PORT || 3000);
