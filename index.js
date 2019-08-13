def678a0c423de23f195b4a650cdbab654209f7c
haoge deng <haoge.deng@gmail.com>
2019-08-13 21:14:06 +1000

    deleted origin headers

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const http = require('http');
 const httpServer = http.createServer();
-var echo = require('socket.io')(httpServer, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-});
+var echo = require('socket.io')(httpServer);
 
 echo.origins('*:*');
 
